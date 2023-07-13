7843218450670d6dfc79be8832aaabcb4924b65c
onuruci <onur.yuce@metu.edu.tr>
2023-07-14 01:42:48 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -39,13 +39,18 @@ function App() {
     console.log(proofInput);
 
     try {
+      console.log(1);
       let { proof, publicSignals } = await makeProof(proofInput, wasmFile, zkeyFile);
+      console.log(1);
       setProof(JSON.stringify(proof, null, 2));
+      console.log(3);
       setSignals(JSON.stringify(publicSignals, null, 2));
+      console.log(4);
       verifyProof(verificationKey, publicSignals, proof).then((_isValid) => {
         console.log("isValid");
         setIsValid(_isValid);
       });
+      console.log(5);
     } catch {
       setProof("");
       setSecret("");
